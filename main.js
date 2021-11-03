253795029e82d4ae1a740a20c5e18480051a6dd7
marijanagardovic <marijana.gardovic99@gmail.com>
2021-11-03 09:08:59 +0100

    fixed price for month/year

--- main.js
+++ main.js
@@ -7,6 +7,48 @@ const price = document.querySelector('#final-price');
 monthArea.addEventListener('click', () => {
     monthArea.classList.toggle('switch-active');
     monthBtn.classList.toggle('switch-btn-active');
+    if(range.value == 1) {
+        if(monthArea.classList.contains('switch-active')){
+            price.textContent = 6;
+        }
+        else {
+            price.textContent = 8;
+        }
+
+        views.textContent = "10k";
+    }
+    if(range.value == 2) {
+        if(monthArea.classList.contains('switch-active')) {
+            price.textContent = 9;
+        } else {
+            price.textContent = 12;
+        }
+        views.textContent = "50k";
+    }
+    if(range.value == 3) {
+        if(monthArea.classList.contains('switch-active')) {
+            price.textContent = 12;
+        } else {
+            price.textContent = 16;
+        }
+        views.textContent = "100k";
+    }
+    if(range.value == 4) {
+        if(monthArea.classList.contains('switch-active')){
+            price.textContent = 18;
+        } else {
+            price.textContent = 24;
+        }
+        views.textContent = "500k";
+    }
+    if(range.value == 5) {
+        if(monthArea.classList.contains('switch-active')){
+            price.textContent = 27;
+        } else {
+            price.textContent = 36;
+        }
+        views.textContent = "1M";
+    }
 })
 
 range.addEventListener('input', () =>{
